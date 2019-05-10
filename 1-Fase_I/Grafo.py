e5c3044edb823876e754aca736d73fd9c6d6c10d
Toni <tboutaour@gmail.com>
2019-05-10 19:58:00 +0200

    Actualización de puntos cercanos
    actualización

--- 1-Fase_I/Grafo.py
+++ 1-Fase_I/Grafo.py
@@ -8,28 +8,38 @@ Created on Wed May  8 18:01:48 2019
 
 import osmnx as ox
 from sklearn.neighbors import KDTree
+import scipy.spatial
 import numpy as np
 import matplotlib.pyplot as plt
 import networkx as nx
 from sklearn.neighbors import KDTree
 import folium
+import gpxpy
 
 class Grafo:
-        def __init__(self, north, south,east,west):
+        def __init__(self, north, south,east,west,p1):
             self.north = north
             self.south = south
             self.east = east
             self.west = west
             self.Grafo = ox.graph_from_bbox(self.north,self.south,self.east,self.west)
-        
-        def GenerarKDtree(self,tolerance):
+            self.p1 = p1
+            
+        def __colorear_rutas(self):
+            return ox.get_edge_colors_by_attr(self.Grafo, attr='length')
+            
+        def GenerarKDtree(self):
             self.nodes, _ = ox.graph_to_gdfs(self.Grafo)
-            self.nodes.head()
-            self.tree = KDTree(self.nodes[['x','y']])
+            self.tree = KDTree(self.nodes[['y', 'x']], metric='euclidean')
 
-        def Muestra(self):
+        def Muestra(self,n):
             G_projected = ox.project_graph(self.Grafo)
-            ox.plot_graph(G_projected)
+            if n == 2:
+                fig, ax = ox.plot_graph(self.Grafo,show=False, close=False, edge_color=(self.__colorear_rutas()))
+            else:
+                fig, ax = ox.plot_graph(G_projected,show=False, close=False, )
+            aux = self.__ImprimirSegmentoRuta("CastilloBellver.gpx")
+            ax = ax.scatter(aux[1], aux[0], c='red', s=20)
             
         def GenerarRutaRandom(self):
             route = nx.shortest_path(self.Grafo, np.random.choice(self.Grafo.nodes), 
@@ -42,18 +52,56 @@ class Grafo:
                         show=False, close=False, 
                         edge_color='black')
             ax.scatter(aux['x'], aux['y'], c='red', s=100)
+            
+        def ObtenerNodoMasCercano(self,nodos,punto):
+            idx_cerc = self.tree.query([punto], k=2, return_distance=False)[0]
+            punt_cerc = self.Grafo.node[(self.nodes.iloc[idx_cerc].index.values[1])]
+            
+            return punt_cerc
+            
+            
+        def __CargarPuntos(self, p):
+            points_a = []
+            #p.simplify()
+            #p.reduce_points(500)
+            for track in p.tracks:  # OJO SOLO HAY UN TRACK
+                for segment in track.segments:  # OJO SOLO HAY UN SEGMENT
+                    for iA in range(0, len(segment.points) - 1):
+                        point = segment.points[iA]
+                        points_a.append([point.longitude, point.latitude])
+            return np.asarray(points_a)
         
-        def ObtenerPuntoMasCercano(self):
-            nodes, _ = ox.graph_to_gdfs(self.Grafo)
-            tree = KDTree(nodes[['y', 'x']], metric='euclidean')
-            aux = self.Grafo.nodes[np.random.choice(self.Grafo.nodes)]
-            aux1 = aux
-            del aux1['osmid']
-            aux1 = tuple(aux1.values())
-            idx_cerc = tree.query([aux1], k=2, return_distance=False)[0]
-            punt_cerc = self.Grafo.node[(nodes.iloc[idx_cerc].index.values[1])]
-            fig, ax = ox.plot_graph(self.Grafo, fig_height=10, fig_width=10, 
-                        show=False, close=False, 
-                        edge_color='black')
-            ax.scatter(aux['x'], aux['y'], c='red', s=50)
-            ax.scatter(punt_cerc['x'], punt_cerc['y'], c='green', s=50)
+        def colorear_rutas(self):
+            return ox.get_edge_colors_by_attr(self.Grafo, attr='length')
+            
+        def __ImprimirSegmentoRuta(self,route):
+            gpx_file = open(route, 'r')
+            gpx = gpxpy.parse(gpx_file)
+            lon=[]
+            lat=[]
+            track = gpx.tracks[0]
+
+            for track in gpx.tracks:
+                for segment in track.segments:
+                    for point in segment.points:
+                        lat.append(point.latitude)
+                        lon.append(point.longitude)
+            coor =[lat,lon] 
+            return coor
+
+
+        def Prueba(self):
+            puntos = np.vstack(self.__CargarPuntos(self.p1))
+            print(puntos)
+            all_points = [puntos]
+            print(all_points)
+            all_pts = np.vstack([np.hstack([a, 0])
+                                  for a in puntos])
+            print(all_pts)
+            points_within_tolerance = self.tree.query_ball_point(puntos, 0.0000009)
+            print(points_within_tolerance)
+            vfunc = np.vectorize(lambda a: np.any( ))
+            self.matches = vfunc(points_within_tolerance)
+        
+
+            
--- 1-Fase_I/Main.py
+++ 1-Fase_I/Main.py
@@ -5,10 +5,37 @@ Created on Wed May  8 17:15:18 2019
 
 @author: tonibous
 """
-
+import gpxpy
 import Grafo as g
+import numpy as np
+import osmnx as ox
 
-grafo = g.Grafo(39.5713,39.5573,2.6257,2.6023)
-#grafo.Muestra()
-grafo.GenerarKDtree(0.5)
-grafo.Prueba()
+
+route1 = "RutaCastilloBellver1.gpx"
+
+f1 = open(route1)
+p1 = gpxpy.parse(f1)
+
+tolerance = 0.0000009
+
+grafo = g.Grafo(39.5713,39.5573,2.6257,2.6023,p1)
+#grafo.Muestra(2)
+#grafo.Grafo.edges()
+#grafo.GenerarKDtree(0.5)
+#grafo.ObtenerPuntoMasCercano()
+#grafo.Prueba()
+
+#ejemplo punto cercano:
+
+aux = grafo.Grafo.nodes[np.random.choice(grafo.Grafo.nodes)]
+punto = aux
+del punto['osmid']
+punto = tuple(punto.values())
+
+grafo.GenerarKDtree()
+punto_cercano=grafo.ObtenerNodoMasCercano(grafo.nodes,punto)
+fig, ax = ox.plot_graph(grafo.Grafo, fig_height=10, fig_width=10, 
+            show=False, close=False, 
+            edge_color='black')
+ax.scatter(punto[1], punto[0], c='red', s=50)
+ax.scatter(punto_cercano['x'], punto_cercano['y'], c='green', s=50)
