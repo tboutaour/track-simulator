7fae0f8062751c21756936f059918d7ee5d2a935
Toni <tboutaour@gmail.com>
2019-11-15 18:53:13 +0100

    Entities: LoaderSaver and implementations

---
+++ entities/GPXLoaderSaver_impl.py
@@ -0,0 +1,13 @@
+import osmnx
+import LoaderSaver
+
+
+class OsmnxLoaderSaver(LoaderSaver):
+    def loadFile(self, input):
+        pass
+
+    def parseFile(self):
+        pass
+
+    def saveFile(self, output, data):
+        pass
