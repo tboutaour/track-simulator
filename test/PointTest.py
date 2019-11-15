8f4b7ce426bbb35930365aec8094a6657c553019
Toni <tboutaour@gmail.com>
2019-11-15 19:57:23 +0100

    Unit Testing Point and Segment

--- test/PointTest.py
+++ test/PointTest.py
@@ -1,20 +1,21 @@
 import unittest
-from Point import Point,Segment
+from entities.TrackPoint_impl import TrackPoint as Point
+from entities.TrackSegment_impl import TrackSegment as Segment
 
 class MyTestCase(unittest.TestCase):
     def test_segment_operations(self):
         point = Point(2, 1)
         point_b = Point(4, 4)
-        segm = Segment([point, point_b])
+        segm = Segment([point.get_latlong(), point_b.get_latlong()])
 
         self.assertEqual(point.get_longitude(), 1.0)
         self.assertEqual(point.haversine_distance(point_b), 400524.4343337236)
         self.assertEqual(segm.get_points(), [(2.0, 1.0), (4.0, 4.0)])
-        self.assertEqual(segm.distance(), 400524.4343337236)
-        segm.add_point(point)
-        segm.add_point(point)
+        self.assertEqual(segm.get_distance(), 400524.4343337236)
+        segm.add_point(point.get_latlong())
+        segm.add_point(point.get_latlong())
         self.assertEqual(segm.get_points(), [(2.0, 1.0), (4.0, 4.0), (2.0, 1.0), (2.0, 1.0)])
-        self.assertEqual(point.calculate_initial_compass_bearing(point_b), 56.20251196993809)
+        self.assertEqual(point.get_bearing(point_b), 56.20251196993809)
 
     def test_point_operations(self):
         point = Point(2, 1)
