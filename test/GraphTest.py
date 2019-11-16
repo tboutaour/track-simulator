ac2fca72f6aff7bf98d75aed536487c9a7595743
Toni <tboutaour@gmail.com>
2019-11-16 14:21:07 +0100

    GraphTest modification

--- test/GraphTest.py
+++ test/GraphTest.py
@@ -5,13 +5,13 @@ import networkx
 
 
 class MyTestCase(unittest.TestCase):
-
     def test_something(self):
         osmnx_bellver_graph = osmnx.graph_from_bbox(39.5713, 39.5573, 2.6257, 2.6023)
-        osmnx_bellver_graph
-        bellver_graph = Graph(osmnx_bellver_graph)
-        #self.assertEqual(bellver_graph.get_edges(), osmnx_bellver_graph.graph.edges(data=True))
-        self.assertEqual(True,True)
+        expected_edges = osmnx_bellver_graph.edges(data=True)
+        bellver_graph = Graph(39.5713, 39.5573, 2.6257, 2.6023)
+        edges = bellver_graph.get_edges()
+        self.assertEqual(edges, expected_edges)
+        self.assertEqual(True, True)
 
 
 if __name__ == '__main__':
